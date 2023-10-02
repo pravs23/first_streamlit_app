5bfa458ee4248f92be915402d1aae633590b4dfb
pravs23 <146384546+pravs23@users.noreply.github.com>
2023-10-02 14:20:13 +0100

    Update streamlit_app.py
    snowflake connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #display the table on the page
 streamlit.dataframe(fruits_to_show)
 
+import snowflake.connector
+
 
 # new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
